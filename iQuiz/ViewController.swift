a87f17cbf7593177190f22d48e3151b27b2552b2
akben <akben@uw.edu>
2025-05-05 17:04:08 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Ben Nguyen on 5/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
